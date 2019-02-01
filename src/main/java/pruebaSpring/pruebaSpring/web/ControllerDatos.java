8a7bec014fb0a8244857613bdde01103b6b83fae
tronxi <sergio.gsanchez97@gmail.com>
2019-02-01 20:31:43 +0100

    enable cors

--- src/main/java/pruebaSpring/pruebaSpring/web/ControllerDatos.java
+++ src/main/java/pruebaSpring/pruebaSpring/web/ControllerDatos.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +24,7 @@ public class ControllerDatos {
 		this.sampleService = sampleService;
 	}
 	
+	@CrossOrigin()
 	@RequestMapping(value = "/dato/{id}", method = RequestMethod.GET)
 	public Dato welcome(@PathVariable("id") String id) {
 		return sampleService.getDatoById(id);
