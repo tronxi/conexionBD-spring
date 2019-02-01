aae73988d998eb71e6a7e8f781179d6f8b08c4d7
tronxi <sergio.gsanchez97@gmail.com>
2019-02-01 21:28:28 +0100

    prueba insert

--- src/main/java/pruebaSpring/pruebaSpring/mapper/MapperDatos.java
+++ src/main/java/pruebaSpring/pruebaSpring/mapper/MapperDatos.java
@@ -16,6 +16,6 @@ public interface MapperDatos {
 	@Select("SELECT id, nombre from tablaPrueba where id = #{id}")
 	Dato getDatoById(String id);
 	
-	@Insert("insert into tablaPrueba (nombre) values ('#{nombre}')")
+	@Insert("insert into tablaPrueba (nombre) values (#{nombre})")
 	void postUser(String nombre);
 }
